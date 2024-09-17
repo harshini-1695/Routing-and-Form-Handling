9b5d6843b48304dc9ab91275772302eb8a665432
harshini-1695 <harshini.ok@arkinfotec.com>
2024-09-17 19:01:37 +0530

    #task-3-Form handling with react hook forms

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,23 +1,33 @@
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as Yup from 'yup';
 import { TextField, Button, Container } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 
 const Login = () => {
     const navigate = useNavigate(),
-        handleSubmit = () => {
+        validationSchema = Yup.object({
+            email: Yup.string().required(),
+            password: Yup.string().required()
+        }),
+        { register, handleSubmit, formState: { isValid } } = useForm({
+            resolver: yupResolver(validationSchema),
+        }),
+        onSubmit = () => {
             navigate('/dashboard');
         };
     return (
         <Container maxWidth="xs">
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={handleSubmit(onSubmit)}>
                 <h1>Login</h1>
-                <TextField
+                <TextField {...register("email")}
                     label="Email"
                     type="email"
                     variant="outlined"
                     fullWidth
                     margin="normal"
                 />
-                <TextField
+                <TextField {...register("password")}
                     label="Password"
                     type="password"
                     variant="outlined"
@@ -29,6 +39,7 @@ const Login = () => {
                     color="primary"
                     fullWidth
                     type='submit'
+                    disabled={!isValid}
                 >
                     Submit
                 </Button>
